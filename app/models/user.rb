dfc1659021cdad3e05c8ddb23e333c6dae3d9087
you0626 <sy0626sk@icloud.com>
2025-02-14 15:15:25 +0900

    usersテーブルの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,12 @@
 class User < ApplicationRecord
+  has_secure_password
+
+  has_many :posts, dependent: :destroy
+  has_many :comments, dependent: :destroy
+  has_many :likes, dependent: :destroy
+  has_many :reports, foreign_key: :reporter_id, dependent: :destroy
+
+  validates :username, presence: true, uniqueness: true, length: { maximum: 50 }
+  validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :password, length: { minimum: 6 }, if: -> { new_record? || !password.nil? }
 end
